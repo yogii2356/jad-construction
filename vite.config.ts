8cad3258ed5d8da5a03a7c6ab2c180d1afd1ef45
yogesh <yoeshbhatt941013@gmail.com>
2026-09-14 10:31:50 +0530

    adding deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,13 +16,17 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/',
+
   plugins: [
     figmaAssetResolver(),
+
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       // Alias @ to the src directory
